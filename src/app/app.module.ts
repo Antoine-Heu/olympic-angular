d8b45ef38090d1f10f9b6561a033b37ff16c3d3c
Antoine Heu <aheu@whispeak.io>
2025-02-24 14:26:43 +0100

    added ng2-charts and updated the code for the new library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
@@ -8,11 +8,27 @@ import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { HeaderComponent } from './header/header.component';
 
 import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { BaseChartDirective, provideCharts, withDefaultRegisterables } from 'ng2-charts';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, NotFoundComponent, HeaderComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, NgxChartsModule],
-  providers: [],
-  bootstrap: [AppComponent, HeaderComponent, HomeComponent, NotFoundComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NotFoundComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgxChartsModule,
+    BaseChartDirective,
+  ],
+  providers: [provideHttpClient(withInterceptorsFromDi()), provideCharts(withDefaultRegisterables())],
+  bootstrap: [
+    AppComponent,
+    HomeComponent,
+    NotFoundComponent,
+    HeaderComponent
+  ],
 })
 export class AppModule {}
